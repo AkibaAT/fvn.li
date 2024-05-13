92d2a2ce37368b62c74e640e8fb6da99cbe3bb4d
Akiba <admin@akiba.games>
2024-05-14 00:02:58 +0200

    Add clickable filters to games table

--- app/Filament/App/Resources/GameResource.php
+++ app/Filament/App/Resources/GameResource.php
@@ -9,6 +9,7 @@ use App\Models\Game;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Filters\Filter;
+use Filament\Tables\Filters\SelectFilter;
 use Filament\Tables\Filters\TernaryFilter;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
@@ -100,41 +101,102 @@ class GameResource extends Resource
                 Tables\Columns\TextColumn::make('languages')
                     ->toggleable(isToggledHiddenByDefault: true),
                 Tables\Columns\IconColumn::make('platform_windows')
+                    ->disabledClick()
+                    ->tooltip('Filter by Windows status')
+                    ->extraAttributes(function (Game $record) {
+                        return [
+                            'wire:click' => '$set("tableFilters.platform_windows.value", "' . (int) $record->platform_windows . '")',
+                            'class' => 'transition hover:text-primary-500 cursor-pointer',
+                        ];
+                    })
                     ->label('Windows')
                     ->boolean(),
                 Tables\Columns\IconColumn::make('platform_linux')
+                    ->disabledClick()
+                    ->tooltip('Filter by Linux status')
+                    ->extraAttributes(function (Game $record) {
+                        return [
+                            'wire:click' => '$set("tableFilters.platform_linux.value", "' . (int) $record->platform_linux . '")',
+                            'class' => 'transition hover:text-primary-500 cursor-pointer',
+                        ];
+                    })
                     ->label('Linux')
                     ->boolean(),
                 Tables\Columns\IconColumn::make('platform_mac')
+                    ->disabledClick()
+                    ->tooltip('Filter by Mac status')
+                    ->extraAttributes(function (Game $record) {
+                        return [
+                            'wire:click' => '$set("tableFilters.platform_mac.value", "' . (int) $record->platform_mac . '")',
+                            'class' => 'transition hover:text-primary-500 cursor-pointer',
+                        ];
+                    })
                     ->label('Mac')
                     ->boolean(),
                 Tables\Columns\IconColumn::make('platform_android')
+                    ->disabledClick()
+                    ->tooltip('Filter by Android status')
+                    ->extraAttributes(function (Game $record) {
+                        return [
+                            'wire:click' => '$set("tableFilters.platform_android.value", "' . (int) $record->platform_android . '")',
+                            'class' => 'transition hover:text-primary-500 cursor-pointer',
+                        ];
+                    })
                     ->label('Android')
                     ->boolean(),
                 Tables\Columns\IconColumn::make('platform_web')
+                    ->disabledClick()
+                    ->tooltip('Filter by Web status')
+                    ->extraAttributes(function (Game $record) {
+                        return [
+                            'wire:click' => '$set("tableFilters.platform_web.value", "' . (int) $record->platform_web . '")',
+                            'class' => 'transition hover:text-primary-500 cursor-pointer',
+                        ];
+                    })
                     ->label('Web')
                     ->boolean(),
                 Tables\Columns\TextColumn::make('game_engine')
+                    ->disabledClick()
+                    ->tooltip('Filter by game engine')
+                    ->extraAttributes(function (Game $record) {
+                        return [
+                            'wire:click' => '$set("tableFilters.game_engine.value", "' . $record->game_engine . '")',
+                            'class' => 'transition hover:text-primary-500 cursor-pointer',
+                        ];
+                    })
                     ->toggleable(isToggledHiddenByDefault: true),
             ])
             ->filters([
                 TernaryFilter::make('nsfw')
                     ->label('NSFW'),
-                Filter::make('windows')
-                    ->toggle()
-                    ->query(fn (Builder $query): Builder => $query->where('platform_windows', true)),
-                Filter::make('linux')
-                    ->toggle()
-                    ->query(fn (Builder $query): Builder => $query->where('platform_linux', true)),
-                Filter::make('mac')
-                    ->toggle()
-                    ->query(fn (Builder $query): Builder => $query->where('platform_mac', true)),
-                Filter::make('android')
-                    ->toggle()
-                    ->query(fn (Builder $query): Builder => $query->where('platform_android', true)),
-                Filter::make('web')
-                    ->toggle()
-                    ->query(fn (Builder $query): Builder => $query->where('platform_web', true)),
+                TernaryFilter::make('platform_windows')
+                    ->label('Windows'),
+                TernaryFilter::make('platform_linux')
+                    ->label('Linux'),
+                TernaryFilter::make('platform_mac')
+                    ->label('Mac'),
+                TernaryFilter::make('platform_android')
+                    ->label('Android'),
+                TernaryFilter::make('platform_web')
+                    ->label('Web'),
+                SelectFilter::make('game_engine')
+                    ->options([
+                        '?' => '?',
+                        'Custom' => 'Custom',
+                        'Flash' => 'Flash',
+                        'GameMaker' => 'GameMaker',
+                        'Godot' => 'Godot',
+                        'heaps.io' => 'heaps.io',
+                        'iFAction Game Maker' => 'iFAction Game Maker',
+                        'Kocho' => 'Kocho',
+                        'RPG Maker MV' => 'RPG Maker MV',
+                        'Ren\'Py' => 'Ren\'Py',
+                        'Twine' => 'Twine',
+                        'TyranoScript' => 'TyranoScript',
+                        'Unity' => 'Unity',
+                        'Visual Novel Maker' => 'Visual Novel Maker',
+                    ])
+                    ->label('Game Engine'),
             ])
             ->defaultSort('version_published_at', 'desc')
             ->paginated([10, 25, 50]);
