178a55f47580d03ce0ae33d6c813a2964bf5f5b3
Akiba <admin@akiba.games>
2025-02-02 01:10:24 +0100

    Increase version_file_types.extension size

---
+++ database/migrations/2025_02_02_000804_increase_extension_column_length_in_version_file_types_table.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('version_file_types', function (Blueprint $table) {
+            $table->string('extension', 100)->change();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('version_file_types', function (Blueprint $table) {
+            $table->string('extension', 10)->change();
+        });
+    }
+};
