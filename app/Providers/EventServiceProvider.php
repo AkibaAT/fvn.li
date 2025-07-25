648312b9762f13ffcc34172212c3c083a8f33b82
Akiba <dev@akiba.at>
2025-07-25 22:49:11 +0200

    Fix exception in add requests

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
-use App\Events\AdditionRequestSubmitted;
-use App\Listeners\SendAdditionRequestDiscordNotification;
 use App\Models\User;
 use App\Observers\UserObserver;
 use Illuminate\Auth\Events\Registered;
@@ -25,9 +23,6 @@ class EventServiceProvider extends ServiceProvider
         ],
         Registered::class => [
             SendEmailVerificationNotification::class,
-        ],
-        AdditionRequestSubmitted::class => [
-            SendAdditionRequestDiscordNotification::class,
         ],
     ];
 
